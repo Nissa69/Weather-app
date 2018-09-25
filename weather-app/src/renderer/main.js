fb8a67dfcc57db0646e951945dba42d7a68d416e
Nissa69 <yazzz01@gmail.com>
2018-09-25 19:37:09 +0200

    add api url

--- weather-app/src/renderer/main.js
+++ weather-app/src/renderer/main.js
@@ -1,11 +1,12 @@
-import Vue from 'vue'
-import axios from 'axios'
-
+import Vue from 'vue';
+import axios from 'axios';
 import App from './App'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://api.openweathermap.org/data/2.5';
 
 /* eslint-disable no-new */
 new Vue({
